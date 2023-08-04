047f5893d9b4ab28c6b6c97ec50fbb6bd827715a
malayadaniGIT <malayadani@gmail.com>
2023-08-04 15:13:53 +0530

    react-jsx-002 done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Reactjsx002 from './Component/ReactJSX002/Reactjsx002';
+
+function App() {
+  return (
+    <div className="App">
+    <Reactjsx002/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/ReactJSX002/Reactjsx002.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./style.css"
+function Reactjsx002() {
+  return (
+    
+      <div className='div'>
+      <h1 id='h1'>The Evolution of <br/>Airbnb's Frontend</h1>
+      <p>@spikebrehm</p>
+      <p>Airbnb</p>
+    </div>
+  )
+}
+
+export default Reactjsx002
